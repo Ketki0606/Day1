ca440d4293b455c528bf411f7d9bdbc496fb3330
Ketki <sakpalketki0606@gmail.com>
2024-04-01 11:41:04 +0530

    File upload

---
+++ EqualString.java
@@ -0,0 +1,11 @@
+
+public class EqualString
+{
+    public static void main(String[] args)
+    {
+        String str1 = "Ketki";
+        String str2 = "sakpal";
+        System.out.println(str1.equals(str2));
+    }
+
+}
